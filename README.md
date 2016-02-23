7cfe33205fd6da1bf54ca1006df1bc9bbe1c353f
tecnoparanoids <tecnoparanoids@radioela.org>
2016-02-23 20:38:40 +0100

    Versión 1.0: * Soporte para Android 6 * Corregidos errores

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# radioslibres
+# Aplicación para Android de las Radios Libres
+
+**Acércate las radios libres a un click.**
+
+Podrás escuchar en directo buena parte de las radios libres del estado español en tu móvil.
+
+La aplicación está hecha con Apache Cordova y se publica bajo licencia GPLv3.
--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+var streamLoading = false;
+ 	
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,18 +36,184 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.receivedEvent("deviceready");
+        
+        console.log("configureEvents");
+	 
+		    var agora = document.getElementById("agora");
+		    agora.addEventListener("click", function(){playStream(this);}, false);
+		    
+		    var arraio = document.getElementById("arraio");
+		    arraio.addEventListener("click", function(){playStream(this);}, false);
+		    
+		    var contrabanda = document.getElementById("contrabanda");
+		    contrabanda.addEventListener("click", function(){playStream(this);}, false);
+
+		    var irola = document.getElementById("irola");
+		    irola.addEventListener("click", function(){playStream(this);}, false);
+
+		    var almaina = document.getElementById("almaina");
+		    almaina.addEventListener("click", function(){playStream(this);}, false);
+
+		    var bronka = document.getElementById("bronka");
+		    bronka.addEventListener("click", function(){playStream(this);}, false);
+
+		    var ela = document.getElementById("ela");
+		    ela.addEventListener("click", function(){playStream(this);}, false);
+
+		    var granja = document.getElementById("granja");
+		    granja.addEventListener("click", function(){playStream(this);}, false);
+
+		    var mai = document.getElementById("mai");
+		    mai.addEventListener("click", function(){playStream(this);}, false);
+
+		    var mutant = document.getElementById("mutant");
+		    mutant.addEventListener("click", function(){playStream(this);}, false);
+
+		    var qk = document.getElementById("qk");
+		    qk.addEventListener("click", function(){playStream(this);}, false);
+
+		    var rsk = document.getElementById("rsk");
+		    rsk.addEventListener("click", function(){playStream(this);}, false);
+
+		    var topo = document.getElementById("topo");
+		    topo.addEventListener("click", function(){playStream(this);}, false);
+
+		    var aktiva = document.getElementById("aktiva");
+		    aktiva.addEventListener("click", function(){playStream(this);}, false);
+
+		    var zintzilik = document.getElementById("zintzilik");
+		    zintzilik.addEventListener("click", function(){playStream(this);}, false);
+		        
+		    var stop = document.getElementById("stop");
+		    stop.addEventListener("click", function(){
+		    streamLoading = false;
+		            var audio = document.getElementById('audio_player');
+		            audio.pause();
+		            audio.src = " ";
+		            var player = document.getElementById("player");
+		            player.style.display = "none";
+		            document.getElementById("radio_list").style.height = "90%";
+		    }, false);
+		        
+		    var mute = document.getElementById("mute");
+		        mute.addEventListener("click", function(){
+		                  var audio = document.getElementById('audio_player');
+		                  audio.muted = !(audio.muted);    
+		                  console.log("mute: " + audio.muted);
+		                  if(audio.muted)     
+		                         mute.className = "muted";
+		                  else
+		                         mute.className = "unmuted";
+		        }, false);
+        
+//        configureEvents();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//        var parentElement = document.getElementById(id);
+//        var listeningElement = parentElement.querySelector('.listening');
+//        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//        listeningElement.setAttribute('style', 'display:none;');
+//        receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
     },
       
 };
+
+
+var playStream = function(link){
+		
+	streamLoading = false;
+
+	var audio = document.getElementById('audio_player');
+	audio.pause();
+	audio.src = " ";
+	console.log("playStream - Radio: " + link.innerHTML);
+	
+    
+    audio.src = link.getAttribute('value');    
+	
+    console.log("playStream: " + audio.src + " - Radio: " + link.innerHTML);
+                
+    var loading = document.getElementById("loading");
+    var player = document.getElementById("player");
+	
+	// Mostramos mensaje de "Cargando..."		
+	loading.innerHTML = 'Cargando audio...';
+	loading.style.display = "block";
+
+	audio.addEventListener("playing", function(){
+        console.log("addEventListener.playing - Radio: " + document.getElementById("radio_name"));
+        streamLoading = true;
+		document.getElementById("radio_name").innerHTML = link.innerHTML;
+		player.style.display = "block";
+		playerShown = true;
+			  
+		document.getElementById("radio_list").style.height = "75%";
+
+		loading.style.display = "none";
+		}, true);
+	
+	audio.addEventListener("durationchange", function(){
+		console.log("addEventListener.durationchange  streamLoading = " + streamLoading);
+//		if(streamLoading == true){
+//			document.getElementById("radio_name").innerHTML = link.innerHTML;
+//			player.style.display = "block";
+//			playerShown = true;
+//			  
+//			document.getElementById("radio_list").style.height = "75%";
+
+//			loading.style.display = "none";
+//		}
+	});
+	
+//	audio.addEventListener("progress", function(){
+//	    console.log("addEventListener.progress - Radio: " + document.getElementById("radio_name"));
+//		}, true);
+	
+	
+	
+	audio.addEventListener('error', onError, true);
+	audio.mozAudioChannelType = 'content';
+	audio.play();
+};
+
+
+
+var onError = function(e) {
+
+	console.log("ERROR AL REPRODUCIR: " + e.target.error.code + " - Audio: " + e.target.src);
+
+    var loading = document.getElementById("loading");
+    var player = document.getElementById("player");
+    
+	switch (e.target.error.code) {
+		case e.target.error.MEDIA_ERR_ABORTED:
+			loading.innerHTML = 'Error: Reproducción abortada';
+		break;
+		case e.target.error.MEDIA_ERR_NETWORK:
+		   loading.innerHTML = 'Error de red. Comprueba la conexión a internet';
+		break;
+		case e.target.error.MEDIA_ERR_DECODE:
+		   loading.innerHTML = 'Error de decodificación';
+		break;
+		case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+		   loading.innerHTML = 'Error. No se ha podido cargar el audio';
+		break;
+		default:
+		   loading.innerHTML = 'Se ha producido un error';
+		break;
+	}
+	streamLoading = false;
+	var audio = document.getElementById('audio_player');
+	audio.pause();
+	document.getElementById("radio_list").style.height = "90%";
+	setTimeout(function(){
+		loading.style.display = "none";
+		player.style.display = "none";
+    },2000); 
+};
+
